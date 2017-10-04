8e110c77f1d539aabfaa302794c731d17417f759
Volodya Skalskyi <volodya.skalskyi@gmail.com>
2017-10-04 21:02:56 +0300

    AI v1

--- public/userBot.js
+++ public/userBot.js
@@ -6,15 +6,37 @@
 function getPlayerMove(data) {
   // TODO : IMPLEMENT THE BETTER STRATEGY FOR YOUR BOT
   var currentPlayer = data.yourTeam.players[data.playerIndex];
+  var direction = currentPlayer.direction, 
+      velocity = currentPlayer.velocity;
+
   var ball = data.ball;
-
   var ballStop = getBallStats(ball, data.settings);
+  
+  var secondZoneStartX = data.settings.field.width / 4;
 
-  var attackDirection = Math.atan2(ballStop.y - currentPlayer.y, ballStop.x - currentPlayer.x - ball.settings.radius);
+  direction = getDirectionTo(currentPlayer, ballStop);
+  velocity = data.settings.player.maxVelocity;
+
+  if (data.playerIndex === 1) {
+    let moveToX = ball.x > secondZoneStartX ? secondZoneStartX : ball.x;
+
+    direction = getDirectionTo(currentPlayer, {x: moveToX, y: ball.y}); 
+  }
+
+  if (ballStop.x < currentPlayer.x) {
+    // do not kick to the my goalpost, move to the position behind the ball
+    const ballRadius = ball.settings.radius;
+    var stopPoint = {
+      x: ballStop.x - ballRadius * 2,
+      y: ballStop.y + (ballStop.y > currentPlayer.y ? - ballRadius : + ballRadius) * 2
+    }
+    direction = getDirectionTo(currentPlayer, stopPoint);
+    velocity = getDistance(currentPlayer, stopPoint);
+  }
 
   return {
-    direction: attackDirection,
-    velocity: currentPlayer.velocity + data.settings.player.maxVelocityIncrement
+    direction: direction,
+    velocity: velocity
   };
 }
 
@@ -34,6 +56,14 @@ function getBallStats(ball, gameSettings) {
 
 function getStopTime(ball) {
   return ball.velocity / ball.settings.moveDeceleration;
+}
+
+function getDirectionTo(startPoint, endPoint) {
+  return Math.atan2(endPoint.y - startPoint.y, endPoint.x - startPoint.x);
+}
+
+function getDistance(point1, point2) {
+  return Math.hypot(point1.x-point2.x, point1.y - point2.y);
 }
 
 onmessage = (e) => postMessage(getPlayerMove(e.data));
